cc125b4148af57991d6f0a6657b39d1f790367df
Waseh Ahmad <ahmadw@lafayette.edu>
2017-02-11 13:54:51 -0500

    Started the project on java. Created the controller main class with basic methods.

---
+++ Controller.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Controller{
+  
+  
+  
+  public void run(){
+    
+    
+    
+  }
+  
+  public static void main(String[] args){
+    
+    Controller parser = new Controller(); //create an instantiation of the controller
+    
+    parser.run(); //run the controller
+    
+    
+    
+  }
+  
+}
