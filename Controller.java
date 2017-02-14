e5390109498daeebf24832dfa1c247e45cd340e7
Waseh Ahmad <ahmadw@lafayette.edu>
2017-02-13 20:52:17 -0500

    Created a readme file and formatting the code

--- Controller.java
+++ Controller.java
@@ -10,21 +10,28 @@ import acm.gui.*;
 
 
 public class Controller extends Program{
+  //set the constants for the frame
   public static final int 
     APPLICATION_WIDTH = 500,
     APPLICATION_HEIGHT = 600;
   
+  //declare the variables of the GUI
   private JTextArea numberArea;
   private JButton generateButton;
   private JTextField urlInput;
   
+  /**
+   * Method to parse the html given the url
+   * @param url the url of the website to parse
+   * @return A linked list of strings containing the phone numbers present on the website. Null if url is not valid
+   */
   public LinkedList<String> parse(String url){
     
     URLParser parser = new URLParser();
     String regex = "((?<![Ff]ax)(?<Valid>[\\s|\\(]*?(\\d{3})([\\)\\s\\-\\.]{0,2})(\\d{3})([\\)\\s\\-\\.]{0,2})(\\d{4})))|((?<=[Ff]ax)(?<invalid>.*?(\\d{3})([\\)\\s\\-\\.]{0,2})(\\d{3})([\\)\\s\\-\\.]{0,2})(\\d{4})))";
-    if(parser.loadURL(url,regex)==false){
+    if(parser.loadURL(url,regex)==false){//if the url is invalid
       return null;
-    }else{
+    }else{//else return a linked list of the phone numbers
       return parser.getMatches();
     }
     
@@ -33,18 +40,19 @@ public class Controller extends Program{
   // draw the gui
   public void init() {
     
+    //set up the GUI
     setTitle("Phone Number Parser");
-    
+    //button
     generateButton = new JButton("Parse");
     generateButton.addActionListener(this);
-    
+    //url input field
     urlInput = new JTextField();
     urlInput.setColumns(20);
-    
+    //display panel for the phone numbers
     numberArea = new JTextArea();
     JScrollPane scrollPane = new JScrollPane(numberArea); 
     numberArea.setEditable(false);
-    
+    //add all to the GUI
     add(generateButton, NORTH);
     add(urlInput, NORTH);
     add(new JScrollPane(numberArea), CENTER);
@@ -53,7 +61,6 @@ public class Controller extends Program{
   
   public void actionPerformed (ActionEvent evt) {
     if (evt.getSource() == generateButton) {  
-      //should return null if url was invalid
       
       LinkedList<String> numbers = parse(urlInput.getText());
       
@@ -65,9 +72,8 @@ public class Controller extends Program{
         for(int i=0;i<numbers.size();i++){
           numberArea.append(numbers.get(i)+"\n");
         }
-        //numberArea.setText(text);
       }else{
-        numberArea.setText("An error occurred. Please change the url or try again");
+        numberArea.setText("An error occurred. Please change the url or try again. Try adding the protocol to the beginning of the url");
       }
       
       
@@ -75,18 +81,5 @@ public class Controller extends Program{
     
   }
     
-    
-    
-    
-    
-//  public static void main(String[] args){
-//    
-//    Controller parser = new Controller(); //create an instantiation of the controller
-//    
-//   // parser.run(); //run the controller
-//    
-//    
-//    
-//  }
     
   }
